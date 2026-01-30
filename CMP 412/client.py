9fdf9190a17fbda797b531021e5dee567c4ddf36
Pelumi Ajayi <ajayioluwapelumi0001@gmail.com>
2026-01-30 11:55:58 +0100

    Implement basic client-server communication
    Added socket connection setup in both client and server scripts. The client now connects to the server and sends a message, while the server accepts the connection and prints the received message before and after decoding.

--- CMP 412/client.py
+++ CMP 412/client.py
@@ -1,7 +1,17 @@
 
 import socket
 
-cs = socket.socket(family=AF_INET, type=socket.SOCK_STREAM)
+cs = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
 print("client start")
+
+
+port = 8000
+ip = socket.gethostname()
+
+cs.connect((ip, port))
+
+msg = "hello server"
+
+cs.send(msg.encode())
 
--- CMP 412/server.py
+++ CMP 412/server.py
@@ -1,7 +1,29 @@
 
+from _socket import socket
 import socket
 
-ss = socket.socket(family=AF_INET, type=socket.SOCK_STREAM)
+ss = socket.socket(family=socket.AF_INET, type=socket.SOCK_STREAM)
 
 print("server start")
 
+#they must have the save port number before both can communicate Client and server
+
+port = 8000
+ip = socket.gethostname()
+
+ss.bind((ip, port))
+
+ss.listen(3)
+
+
+con, addr = ss.accept()
+
+print("address client cont {0} and port no {1}".format(addr[0], addr[1]))
+
+data = con.recv(1024)
+
+print("message from client before decoding", data)
+
+data = data.decode()
+
+print("message from client after decoding", data)
