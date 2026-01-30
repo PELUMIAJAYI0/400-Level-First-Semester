0dd5639c721553100f4d0528744e85f47e9283b8
Pelumi Ajayi <ajayioluwapelumi0001@gmail.com>
2026-01-30 11:13:04 +0100

    Add initial client and server socket scripts
    Created basic client.py and server.py files with socket initialization and startup messages for both client and server.

---
+++ CMP 412/client.py
@@ -0,0 +1,7 @@
+
+import socket
+
+cs = socket.socket(family=AF_INET, type=socket.SOCK_STREAM)
+
+print("client start")
+
---
+++ CMP 412/server.py
@@ -0,0 +1,7 @@
+
+import socket
+
+ss = socket.socket(family=AF_INET, type=socket.SOCK_STREAM)
+
+print("server start")
+
